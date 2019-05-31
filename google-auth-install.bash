0b0638c2a7365f428136a7a5e627b33c57e68274
Dmitriy Prigoda <46213676+D34m0nN0n3@users.noreply.github.com>
2019-05-31 16:49:18 +0300

    Update google-auth-install.bash

--- google-auth-install.bash
+++ google-auth-install.bash
@@ -10,39 +10,63 @@ export LANG=en_US.UTF-8
 
 PACKAGES=( bash )
 
+set -u          # Detect undefined variable
+set -o pipefail # Return return code in pipeline fails
+
 if [[ $EUID -ne 0 ]]; then
    echo "[-] This script must be run as root" 1>&2
    exit 1
 fi
 
+# Detecting login user info
+[[ -n "${USER:-}" && -z "${SUDO_USER:-}" ]] && login_user="${USER:-}" || login_user="${SUDO_USER:-}"
+[[ -z "${login_user}" ]] && login_user=$(logname 2> /dev/null)
+
+login_user_home="/home/${login_user}"
+login_ssh_authorize_path=${login_ssh_authorize_path:-"${login_user_home}/.ssh/authorized_keys"}
+
+# gal - Google Authenticator Libpam
+gal_decompress_dir='/tmp/google-authenticator-libpam'
+gal_installation_dir='/opt/googleAuthenticator'
+
 ### Compile Google Authenticator
 yum remove google-authenticator -y;
 yum install git-core qrencode pam-devel make gcc wget autoreconf unzip autoconf automake libtool -y;
 
-git clone https://github.com/google/google-authenticator-libpam.git
+local git_link=${git_link:-"https://github.com/google/google-authenticator-libpam"}
+git clone -q "${git_link}.git" "${gal_decompress_dir}"
 
-[[ -d '/opt/googleAuthenticator' ]] && rm -rf /opt/googleAuthenticator
-mkdir -pv /opt/googleAuthenticator
+[[ -d ${gal_installation_dir} ]] && rm -rf ${gal_installation_dir} && mkdir -pv ${gal_installation_dir}
 
-./bootstrap.sh ;
-./configure  --prefix=/opt/googleAuthenticator ;
-make && make install ;
+pushd ${gal_decompress_dir}
+./bootstrap.sh ; ./configure  --prefix=/opt/googleAuthenticator ; make -j 3 && make install ;
+
 
 # Install Google Authenticator
 unlink /usr/bin/google-authenticator ;
 unlink /lib64/security/pam_google_authenticator.so ;
 unlink /lib64/security/pam_google_authenticator.la ;
-ln -s /usr/local/bin/google-authenticator /usr/bin/google-authenticator ;
-ln -s /usr/local/lib/security/pam_google_authenticator.so /lib64/security/pam_google_authenticator.so ;
-ln -s /usr/local/lib/security/pam_google_authenticator.la /lib64/security/pam_google_authenticator.la ;
+ln -fs ${gal_installation_dir}/bin/google-authenticator /usr/bin/google-authenticator ;
+ln -fs ${gal_installation_dir}/lib/security/pam_google_authenticator.so /lib64/security/pam_google_authenticator.so ;
+ln -fs ${gal_installation_dir}/lib/security/pam_google_authenticator.la /lib64/security/pam_google_authenticator.la ;
 
 # Config SSH Daemon
 sed -i '/#%PAM/a auth\ \ \ \ \ \ \ required\ \ \ \ \ pam_google_authenticator.so nullok' /etc/pam.d/sshd ;
+if [ -f "${login_ssh_authorize_path}" ]; then
+   sed -i -r '/auth[[:space:]]+substack[[:space:]]+password-auth/s/^/#/' /etc/pam.d/sshd ;
+fi
+
 sed -i 's/#PermitRootLogin\ yes/PermitRootLogin\ no/g' /etc/ssh/sshd_config ;
 sed -i 's/#UseDNS\ yes/UseDNS\ no/g' /etc/ssh/sshd_config ;
 sed -i -r 's@(ChallengeResponseAuthentication) no@\1 yes@g' /etc/ssh/sshd_config ;
 
+
+
 service sshd restart ;
 
+exec sudo -u ${USERNAME} /bin/sh - << EOF
 echo 'y' | google-authenticator -t -d -f -Q UTF8 -r 3 -R 30 2>&1 > ~/google_authenticator
+EOF
+
 # EnD
+exit
