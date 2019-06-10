940f3304234e1baaddce34451c521ec3b852a5bb
Dmitriy Prigoda <46213676+D34m0nN0n3@users.noreply.github.com>
2019-06-10 13:51:00 +0300

    Update google-auth-install.bash

--- google-auth-install.bash
+++ google-auth-install.bash
@@ -3,6 +3,8 @@
 # This script is free software: Everyone is permitted to copy and distribute verbatim copies of 
 # the GNU General Public License as published by the Free Software Foundation, either version 3
 # of the License, but changing it is not allowed.
+# Installing Google Authenticator on Amazon Linux and RHEL\CentOS.
+# Thank you for helping NikSonn (https://github.com/NikSonn).
 
 # Export LANG so we get consistent results
 # For instance, fr_FR uses comma (,) as the decimal separator.
@@ -64,24 +66,24 @@ gal_decompress_dir='/tmp/google-authenticator-libpam'
 gal_installation_dir='/opt/googleAuthenticator'
 
 ### Compile Google Authenticator
-yum remove google-authenticator -y;
-yum install git-core qrencode pam-devel make gcc wget autoreconf unzip autoconf automake libtool -y;
+yum remove google-authenticator -y > /dev/null 2>&1 ;
+yum install git-core qrencode pam-devel make gcc wget autoreconf unzip autoconf automake libtool -y > /dev/null 2>&1 ;
 
 git_link=${git_link:-"https://github.com/google/google-authenticator-libpam"}
-git clone -q "${git_link}.git" "${gal_decompress_dir}"
+git clone -q "${git_link}.git" "${gal_decompress_dir}" > /dev/null 2>&1
 
 [[ -d ${gal_installation_dir} ]] && rm -rf ${gal_installation_dir} && mkdir -pv ${gal_installation_dir}
 
 pushd ${gal_decompress_dir}
-./bootstrap.sh ; ./configure  --prefix=/opt/googleAuthenticator ; make -j 3 && make install ;
+./bootstrap.sh ; ./configure  --prefix=/opt/googleAuthenticator ; make -j 3 && make install > /dev/null 2>&1 ;
 
 # Setup Google Authenticator
-unlink /usr/bin/google-authenticator ;
-unlink /lib64/security/pam_google_authenticator.so ;
-unlink /lib64/security/pam_google_authenticator.la ;
-ln -fs ${gal_installation_dir}/bin/google-authenticator /usr/bin/google-authenticator ;
-ln -fs ${gal_installation_dir}/lib/security/pam_google_authenticator.so /lib64/security/pam_google_authenticator.so ;
-ln -fs ${gal_installation_dir}/lib/security/pam_google_authenticator.la /lib64/security/pam_google_authenticator.la ;
+unlink /usr/bin/google-authenticator > /dev/null 2>&1 ;
+unlink /lib64/security/pam_google_authenticator.so > /dev/null 2>&1 ;
+unlink /lib64/security/pam_google_authenticator.la > /dev/null 2>&1 ;
+ln -fs ${gal_installation_dir}/bin/google-authenticator /usr/bin/google-authenticator > /dev/null 2>&1 ;
+ln -fs ${gal_installation_dir}/lib/security/pam_google_authenticator.so /lib64/security/pam_google_authenticator.so > /dev/null 2>&1 ;
+ln -fs ${gal_installation_dir}/lib/security/pam_google_authenticator.la /lib64/security/pam_google_authenticator.la > /dev/null 2>&1 ;
 if [ $? -ne 0 ]; then
     print_FAIL
     exit 1
@@ -93,10 +95,10 @@ fi
 # Install Google Authenticator Libpam
 function install_gal_repo {
 pad "Install Google Authenticator Libpam:"
-yum install google-authenticator -y > /dev/null 2>&1 || ( echo "Connect EPEL repository for google-authenticator installation" && exit 1 )
+yum install google-authenticator -y > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     print_FAIL
-    exit 1
+    echo -n "Connect EPEL repository for google-authenticator installation" && exit 1
 else
     print_SUCCESS
 fi
@@ -105,7 +107,7 @@ fi
 # Config SSH Daemon
 function config_sshd {
 pad "Configuring SSH Daemon:"
-awk 'FNR==NR{ if (/auth/) p=NR; next} 1; FNR==p{ print "auth       required     pam_google_authenticator.so nullok" }' /etc/pam.d/sshd ;
+sed -i -e '/^auth\ \ \ \ \ \ \ /a auth\ \ \ \ \ \ \ required\ \ \ \ \ pam_google_authenticator.so nullok' /etc/pam.d/sshd ;
 sed -i 's/#PermitRootLogin\ yes/PermitRootLogin\ no/g' /etc/ssh/sshd_config ;
 sed -i 's/#UseDNS\ yes/UseDNS\ no/g' /etc/ssh/sshd_config ;
 sed -i -r 's@(ChallengeResponseAuthentication) no@\1 yes@g' /etc/ssh/sshd_config ;
@@ -127,7 +129,7 @@ fi
 
 # Config Google Authenticator
 function config_gal_git {
-pad "Configuring Google Authenticator:"
+echo "Configuring Google Authenticator:"
 sudo -u ${login_user} google-authenticator -t -d -f -Q UTF8 -C -r 3 -R 30 -w 17 -e 10 -s ${login_user_home}/.google_authenticator && systemctl restart sshd.service ;
 if [ $? -ne 0 ]; then
     print_FAIL
@@ -139,7 +141,7 @@ fi
 
 # Config Google Authenticator
 function config_gal_repo {
-pad "Configuring Google Authenticator:"
+echo "Configuring Google Authenticator:"
 sudo -u ${login_user} google-authenticator -t -d -f -Q UTF8 -r 3 -R 30 -w 17 -e 10 -s ${login_user_home}/.google_authenticator && systemctl restart sshd.service ;
 if [ $? -ne 0 ]; then
     print_FAIL
@@ -149,15 +151,11 @@ else
 fi
 }
 
-if [[ ! (-s '/etc/os-release' || -s '/etc/redhat-release') ]]; then
-     OS=`cat /etc/os-release | grep ^NAME | cut -f2 -d\"`
-     if [[ "$OS" == "Amazon Linux" ]]; then
-       install_gal_git && config_sshd && config_sshd_authmethods && config_gal_git
-     elif [[ "$OS" == "CentOS Linux" ]] || [[ "$OS" == "Red Hat Enterprise Linux Server" ]]; then
-       install_gal_repo && config_sshd && config_gal_repo
-fi
-else
-   echo -e "Sorry: this script fail to recognize your system!"
+OS=`cat /etc/os-release | grep ^NAME | cut -f2 -d\"`
+if [[ "$OS" == "Amazon Linux" ]]; then
+  install_gal_git && config_sshd && config_sshd_authmethods && config_gal_git
+elif [[ "$OS" == "CentOS Linux" ]] || [[ "$OS" == "Red Hat Enterprise Linux Server" ]]; then
+  install_gal_repo && config_sshd && config_gal_repo
 fi
 
 # EnD
