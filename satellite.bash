de4039e5d73dee851e84a5888dfbfdb8e26bab91
Dmitriy Prigoda <46213676+D34m0nN0n3@users.noreply.github.com>
2019-08-27 15:18:45 +0300

    Create satellite.bash

---
+++ satellite.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Copyright (C) 2019 Dmitriy Prigoda <deamon.none@gmail.com> 
+# This script is free software: Everyone is permitted to copy and distribute verbatim copies of 
+# the GNU General Public License as published by the Free Software Foundation, either version 3
+# of the License, but changing it is not allowed.
+# This scripr the client registration on katello servers.
